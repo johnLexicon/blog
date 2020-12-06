cfe9ba138b0b6c7caa8f3706d6d152f7d1f4e950
john lundgren <john.lundgren.lexicon@gmail.com>
2020-12-06 19:12:53 +0100

    Extracted Previews card to its own component.

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Grid column -->
+  <div class="col-lg-12 col-md-12">
+    <!--Panel-->
+    <div class="card text-center">
+      <div class="card-body">
+        <h4 class="card-title">{{ post.title }}</h4>
+        <p class="card-text">{{ post.preview }}</p>
+        <router-link
+          :to="{ name: 'BlogPost', params: { postId: post._id } }"
+          class="btn btn-success btn-sm"
+          >Read more</router-link
+        >
+      </div>
+    </div>
+    <!--/.Panel-->
+  </div>
+  <!-- Grid column -->
+  <!-- Grid row -->
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: ['post']
+};
+</script>
+
+<style>
+</style>
--- src/views/Previews.vue
+++ src/views/Previews.vue
@@ -7,31 +7,20 @@
     </div>
     <!-- Grid row -->
     <div v-else class="row my-4" v-for="post in filteredPosts" :key="post._id">
-      <!-- Grid column -->
-      <div class="col-lg-12 col-md-12">
-        <!--Panel-->
-        <div class="card text-center">
-          <div class="card-body">
-            <h4 class="card-title">{{ post.title }}</h4>
-            <p class="card-text">{{ post.preview }}</p>
-            <router-link
-              :to="{ name: 'BlogPost', params: { postId: post._id } }"
-              class="btn btn-success btn-sm"
-              >Read more</router-link
-            >
-          </div>
-        </div>
-        <!--/.Panel-->
-      </div>
-      <!-- Grid column -->
+      <Card :post="post" />
     </div>
-    <!-- Grid row -->
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Card from '@/components/Card';
+
 export default {
+  name: 'Previews',
+  components: {
+    Card
+  },
   created() {
     this.retrievePosts();
   },
